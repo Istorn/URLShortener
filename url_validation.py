1f2960cba2e6bae6b2bd775a6c550097d3e5620c
Lorenzo Neri <istorn.one@gmail.com>
2024-02-01 18:37:06 +0100

    based on urrllib

--- url_validation.py
+++ url_validation.py
@@ -1,56 +1,18 @@
+from urllib.parse import urlparse, parse_qs, urlencode
+
+
 def is_valid_url(url_to_check):
-    if not isinstance(url_to_check, str):
-        return False
-    
-    if (check_base_url(url_to_check)):
-        # Check if the URL contains a GET parameters part
-        if ((url_to_check.index("?"))>0) and (len(url_to_check.split("?")[1])):
-            get_parameters=url_to_check.split("?")[1]
-            if (check_get_parameters(get_parameters)):
-                return True
-            else:
-                return False
+    checked_result=urlparse(url_to_check)
+    # check if firstly the URL has a valid protocol and valid base URL
+    if len(checked_result.scheme)=="https://" and len(checked_result.netloc)>0:
+        # check if the URL has GET parameters
+        if len(checked_result.query)>0:
+            # We sort by key and reconstruct the string sorted by get parameters name
+            sorted_get_params=dict(sorted(parse_qs(urlparse().query).items()))
+            urlencode(sorted_get_params, doseq=True)
         else:
-            return True
-    else:
-        return False
+            sorted_get_params=""
+        # Prepare the element as a dict
+        return {"baseURL":checked_result.netloc,"path": checked_result.path, "getParams":sorted_get_params}
+    return False
 
-def check_base_url(url_to_check):
-    url_compliant_chars = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-")
-     # Check if base URL is HTTP compliant for the protocol
-    if (not url_to_check.startswith("http://")) or (not url_to_check.startswith("https://")):
-        return False
-    if url_to_check.index("https://")>0:
-        base_url = url_to_check[len("http://"):]
-    else:
-        base_url = url_to_check[len("https://"):]
-    
-    # Check if base URL is HTTP compliant for the URL structure
-    if not all(char in url_compliant_chars for char in base_url):
-        return False
-    if ".." in base_url:
-        return False
-    
-    
-
-
-def check_get_parameters(get_parameters):
-    # check if the syntax of GET parameters is HTTP compliant
-    if "??" in get_parameters or "==" in get_parameters or "&&" in get_parameters:
-        return False
-    
-    # split by ampersand
-    splitted_get_parameters=get_parameters.split("&")
-    get_param_compliant_chars= set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_~.")
-
-    # check if GET parameters are HTTP compliant one by one: getParameterName=getParameterValue and characters compliant
-    for current_get_parameter in splitted_get_parameters:
-        current_get_name_value=current_get_parameter.split("=")
-        if (len(current_get_name_value)!=2) or (not(all(char in get_param_compliant_chars for char in current_get_parameter))):
-            return False
-    
-    return True
-
-    
-    
-    
