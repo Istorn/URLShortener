8e326e309dd3c542050614960209a1863f8516cc
Lorenzo Neri <istorn.one@gmail.com>
2024-02-02 14:39:53 +0100

    create function to check the first step of shortened, made as a class

--- shorten_composer/shortener.py
+++ shorten_composer/shortener.py
@@ -1,39 +1,69 @@
 from database_manager import UrlDBHandler
 from datetime import datetime, timedelta
 
-# Get the original URL from the database
-def compose_original_url(shortened_url:str, db_connection:UrlDBHandler,garbage_TTL):
-    result=db_connection.get_original_url(shortened_url)
 
-    # It means that exists in the DB
-    if result != "not_found":
-        expiration_datetime = result["TTLDateTime"] + timedelta(seconds=garbage_TTL)
+class Shortener:
+    def __init__(self, db_connection: UrlDBHandler, garbage_TTL):
+        self.db_connection = db_connection
+        self.garbage_TTL = garbage_TTL
 
-        # If the link is still valid return the orginal one otherwise communicate the expiration happened
-        if expiration_datetime > datetime.now():
-            return result["original_URL"]
+    # Get the original URL from the database
+    def compose_original_url(self, shortened_url: str):
+        result = self.db_connection.get_original_url(shortened_url)
+
+        # It means that exists in the DB
+        if result != "not_found":
+            expiration_datetime = result["TTLDateTime"] + timedelta(seconds=self.garbage_TTL)
+
+            # If the link is still valid return the original one otherwise communicate the expiration happened
+            if expiration_datetime > datetime.now():
+                return result["original_URL"]
+            else:
+                return "link_expired"
+
+        return "link_not_found"
+
+    # Shorten the URL
+    def shorten_url(self, decomposed_url):
+        # Check if the decomposed_url is already existing on the DB
+        element_key=self.alphanumerical_to_element_key(decomposed_url)
+        existing_shortened = self.db_connection.check_existing_shortened(element_key)
+        if existing_shortened is not None:
+             # It means that exists in the DB
+            if existing_shortened != "not_found":
+                expiration_datetime = existing_shortened["TTLDateTime"] + timedelta(seconds=self.garbage_TTL)
+
+                # In this case, we renew its own TTL
+                if expiration_datetime > datetime.now():
+                    self.db_connection.renew_TTLDateTime_document("shortened",element_key)ù
+                
+                # Return the result
+                existing_shortened["original_URL"]
         else:
-            return "link_expired"
-    
-    return "link_not_found"
 
-def shorten_url(decomposed_url, db_connection:UrlDBHandler):
-    # Check if the decomposed_url is already existing on the DB
+            # Check if the baseURL is already existing
 
-    # Check if the baseURL is already existing
+            # Check if the path is already existing
 
-    # Check if the path is already existing
+            # Check if the GET parameters are already existing
 
-    # Check if the GET parameter are already existing
+    # Generate the equivalent alphanumerical string by the given numeric key
+    def element_key_to_alphanumerical(self, element_key):
+        translating_characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+        translated_key = ""
 
+        for element in element_key:
+            translated_key += translating_characters[element]
 
+        return translated_key
 
-# Generate the equivalent alphanumerical string by the given numeric key
-def translatorKey(elementKey):
-    
-    translatingCharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-    translatedKey=""
-    for element in elementKey:
-        equivalentElement+=translatingCharacters[element]
-    
-    return translatedKey
+    # Opposite function
+    def alphanumerical_to_element_key(self, alphanumerical_element):
+        translating_characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+        splitted_alphanumerical = list(alphanumerical_element)
+        element_key = ""
+
+        for splitted in splitted_alphanumerical:
+            element_key += translating_characters.index(splitted)
+
+        return element_key
